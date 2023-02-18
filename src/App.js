e212a6062463128f914c92a84cd6c5d1327c1dde
sparkle_biswas <sparkleb04@gmail.com>
2023-02-18 18:29:20 -0500

    navigates to courses and settings pages - 1.5 hours

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import NavBar from './Components/NavBar';
+import Courses_Student from './Pages/Courses_Student';
+import Settings from './Pages/Settings';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div ClassName='App'>
-      <NavBar></NavBar>
+      <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/Courses_Student" element={<Courses_Student/>}/>
+        <Route exact path="/Settings" element={<Settings/>}/>
+      </Routes>
+      </Router>
+      
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,35 +1,45 @@
-import React, {useState} from 'react'
-import logo from '../images/campuslink_logo.jpg'
-import menu_logo from '../images/menu_logo.jpg'
-import settings_logo from '../images/settings_logo.jpg'
-import notification_logo from '../images/notification_logo.jpg'
-import user_logo from '../images/user_logo.jpg'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../Images/campuslink_logo.jpg';
+import menu_logo from '../Images/menu_logo.jpg';
+import settings_logo from '../Images/settings_logo.jpg';
+import notification_logo from '../Images/notification_logo.jpg';
+import user_logo from '../Images/user_logo.jpg';
 import "./NavBar.css";
 
 function NavBar() {
-  return (
-    <>
-    <nav className="navbar">
-        <div className = "navbar-container">
-            <div className = "logo-container">
-                <img src={logo} className = "logo" alt="logo"/>
+    function sayHello() {
+        alert('Courses!');
+    }  
+    return (
+        <>
+        <nav className="navbar">
+            <div className = "navbar-container">
+                <div className = "logo-container">
+                    <img src={logo} className = "logo" alt="logo"/>
+                </div>
+                <div className = "menu-logo-container">
+                    <Link to="/Courses_Student">
+                        <img src={menu_logo} className = "menu_logo" alt="menu"/>
+                    </Link>
+                </div>
+                <div className = "notification-logo-container">
+                    <img src={notification_logo} className = "notification_logo" alt="notifications"/>
+                </div>
+                <div className = "settings-logo-container">
+                    <Link to="/Settings">
+                        <img src={settings_logo} className = "settings_logo" alt="settings"/> 
+                    </Link>
+                </div>
+                <div className = "user-logo-container">
+                    <img src={user_logo} className = "user_logo" alt="user profile"/> 
+                </div>
+                    
             </div>
-            <div className = "menu-logo-container">
-                <img src={menu_logo} className = "menu_logo" alt="menu"/>
+            <div className="seperation-bar">
             </div>
-            <div className = "notification-logo-container">
-                <img src={notification_logo} className = "notification_logo" alt="notifications"/>
-            </div>
-            <div className = "settings-logo-container">
-                <img src={settings_logo} className = "settings_logo" alt="settings"/> 
-            </div>
-            <div className = "user-logo-container">
-                <img src={user_logo} className = "user_logo" alt="user profile"/> 
-            </div>
-                 
-        </div>
-    </nav>
-    </>
+        </nav>
+        </>
   )
 }
 
