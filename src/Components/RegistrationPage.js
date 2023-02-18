650e9641b109dba7833de4600ede0cf6f6643bda
Rudis Sprenne <70488838+rsprenne@users.noreply.github.com>
2023-02-17 21:07:39 -0500

    Created first iteration of registration form

--- src/Components/RegistrationPage.js
+++ src/Components/RegistrationPage.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import banner from '../images/campuslink_banner.jpg'
+import RegistrationForm from "./RegistrationForm";
 import "./RegistrationPage.css"
 
 function RegistrationPage() {
@@ -7,6 +8,8 @@ function RegistrationPage() {
         <div className="RegistrationPage">
             <header className="RegistrationPage-header">
                 <img src={banner} className="banner" alt="banner"/>
+                <h2>Register</h2>
+                <RegistrationForm></RegistrationForm>
             </header>
         </div>
     )
