606a4ec1be904c2ff03a24375a6343e50b3b6f51
Rudis Sprenne <70488838+rsprenne@users.noreply.github.com>
2023-02-17 23:15:18 -0500

    Second iteration of Registration Form using props

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -1,34 +1,90 @@
-import React, {useState} from "react";
+import React, { Component } from "react";
 
-const initialValues = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    username: "",
-    password: "",
-};
+class RegistrationForm extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            username: '',
+            password: ''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-export default function RegistrationForm() {
-    const [values, setValues] = useState(initialValues);
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
 
-    const handleInputChange = (e) => {
-        const {name, value} = e.targer;
-        setValues({
-            ...values,
-            [name]: value,
+        this.setState({
+            [name]: value
         });
-    };
+    }
 
+    handleSubmit(event){
+        alert('First Name: ' + this.state.firstName + 'Last Name: ' + this.state.lastName + 'Email: ' + this.state.email + 'Username: ' + this.state.username + 'Password: ' + this.state.password)
+        event.preventDefault();
+    }
+    
+    render() {
     return(
-        <form>
-            <label>
-                First Name:
-                <input
-                    value={values.firstName}
-                    onChange={handleInputChange}
-                    label="First Name"
-                    />
-            </label>
-        </form>
-    )
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    First Name:
+                    <input
+                        name="firstName"
+                        type="text"
+                        value={this.state.firstName}
+                        onChange={this.handleInputChange}
+                        />
+                </label>
+                <br/>
+                <label>
+                    Last Name:
+                    <input
+                        name="lastName"
+                        type="text"
+                        value={this.state.lastName}
+                        onChange={this.handleInputChange}
+                        />
+                </label>
+                <br/>
+                <label>
+                    Email:
+                    <input
+                        name="email"
+                        type="text"
+                        value={this.state.email}
+                        onChange={this.handleInputChange}
+                        />
+                </label>
+                <br/>
+                <label>
+                    Username:
+                    <input
+                        name="username"
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        />
+                </label>
+                <br/><label>
+                    Password:
+                    <input
+                        name="password"
+                        type="text"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        />
+                </label>
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
 }
+
+export default RegistrationForm
