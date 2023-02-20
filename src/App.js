0c9d3602ff43d6c543cfc24adcca58af40e4dc59
black-tul1p <divay7@gmail.com>
2023-02-19 21:20:28 -0500

    Add backend testing code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Components/Login";
+import handleSubmitTest from "./Backend/handleSubmit-test";
+import { useRef } from "react";
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+  // Backend testing code
+  const testBackend = false;
+  const dataRef = useRef();
+  const submithandler = (e) => {
+    e.preventDefault();
+    handleSubmitTest(dataRef.current.value);
+    dataRef.current.value = "";
+  };
 
-  return <Login />;
+  // For Backend testing only, will remove later
+  if (!testBackend) return <Login />;
+  return (
+    <div className="App">
+      <form onSubmit={submithandler}>
+        <input type="text" ref={dataRef} />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
