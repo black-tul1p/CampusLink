9fa47e024e4cbaab5a871782924745669dc1dc62
xdextra <53661203+xdextra@users.noreply.github.com>
2023-02-24 17:03:06 +0000

    created css file, added test button, committing to save before more changes

--- src/Components/CourseContent.js
+++ src/Components/CourseContent.js
@@ -1,10 +1,12 @@
-import React from "react"
-import "./CourseContent.css"
+import React from 'react';
+import CourseContentCSS from "./CourseContent.css"
 
 
 export default function CourseContent() {
-    return (
-        <h1>test...again</h1>
-    )
-    
+
+
+    return <div className = "CCtest">
+            <button className="CCtest">ClickMe 
+            </button>
+    </div>
 }
