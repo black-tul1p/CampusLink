d589644d795608f787c0052e6e73e515c929d4c7
xdextra <53661203+xdextra@users.noreply.github.com>
2023-02-24 03:20:53 +0000

    slight more set up to start work on front end of CourseContent page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 //import NavBar from './Components/NavBar';
-//import "./App.css";
+import "./App.css";
 //import Login from "./Components/Login";
 import handleSubmitTest from "./Backend/handleSubmit-test";
 import { useRef } from "react";
 import CourseContent from "./Components/CourseContent"
+import NavBar from "./Components/NavBar"
 
 function App() {
   /* Backend testing code
