3c799831d99c32040bbe6ce39eea41ff49f9f663
black-tul1p <divay7@gmail.com>
2023-02-26 17:31:59 -0500

    Fixed typo

--- src/Backend/firebase.js
+++ src/Backend/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
+  apiKey: process.env.REACT_APP_Firebase_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
