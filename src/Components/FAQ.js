75bf81816c6cd6f8b496df46c9423dc6805f2234
black-tul1p <divay7@gmail.com>
2023-02-26 22:56:10 -0500

    Improved UI

--- src/Components/FAQ.js
+++ src/Components/FAQ.js
@@ -6,12 +6,59 @@ import {
   AccordionSummary,
   AccordionDetails,
   Box,
+  CircularProgress,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { fetchFAQ } from "../Backend/faq";
+import styled from "@emotion/styled";
 
 export default function FAQ() {
-  // Sample Q&As
+  const StyledContainer = styled(Container)`
+    padding-top: 40px;
+    padding-bottom: 40px;
+  `;
+
+  const StyledTitle = styled(Typography)`
+    text-align: center;
+    margin-bottom: 40px;
+    color: #fff;
+  `;
+
+  const StyledBox = styled(Box)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60vh;
+  `;
+
+  const AccordionStyled = styled(Accordion)`
+    margin-bottom: 16px;
+    background-color: #132b3d;
+    color: #fff;
+    box-shadow: none;
+    border-radius: 1em;
+    border: 1px solid #fff;
+    overflow: hidden;
+  `;
+
+  const StyledSummary = styled(AccordionSummary)`
+    && {
+      background-color: #132b3d;
+      color: #fff;
+      border-radius: 1em;
+    }
+  `;
+
+  const StyledDetails = styled(AccordionDetails)`
+    background-color: #1e4258;
+    color: #fff;
+    border-top: 1px solid #fff;
+  `;
+
+  const ExpandMoreIconStyled = styled(ExpandMoreIcon)`
+    color: #fff;
+  `;
+
   const defFAQ = [
     {
       id: 1,
@@ -23,53 +70,60 @@ export default function FAQ() {
       id: 2,
       question: "How do I add a course to my CampusLink dashboard?",
       answer:
-        "If you are a student, you will be added to a course by your professor. If you are an instructor, you can create a new course from the dashboard.",
+        "To add a course to your CampusLink dashboard, first make sure you are logged in to your account. Then, click on the 'Courses' tab in the top navigation menu. From there, you can search for a course by name or course code, and click the 'Add Course' button to add it to your dashboard.",
     },
     {
       id: 3,
       question: "How do I upload an assignment on CampusLink?",
       answer:
-        "To upload an assignment on CampusLink, navigate to the 'Assignments' tab in your course and select the appropriate assignment. Then, click the 'Upload' button and follow the instructions to upload your file. You can also add any additional comments or notes for your instructor if needed.",
+        "To upload an assignment on CampusLink, navigate to the 'Assignments' tab in your dashboard and select the appropriate assignment. Then, click the 'Upload' button and follow the instructions to upload your file. You can also add any additional comments or notes for your instructor if needed.",
     },
     {
       id: 4,
       question: "How do I contact my instructor on CampusLink?",
       answer:
-        "To contact your instructor on CampusLink, navigate to the 'Classlist' page and click on the instructor's email to send them a mail.",
+        "To contact your instructor on CampusLink, navigate to the 'Inbox' tab in your dashboard and select the appropriate conversation. You can send a new message or reply to an existing message in the conversation. You can also see your conversation history and any attachments or files that have been shared.",
     },
   ];
 
   const [faq, setFAQ] = useState(defFAQ);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchFAQ()
       .then((res) => {
-        setFAQ([...faq, res]);
+        setFAQ([...faq, ...res]);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error.message);
+        setLoading(false);
       });
   }, []);
 
   return (
-    <Box>
-      <Container maxWidth="md">
-        <Typography variant="h4" gutterBottom>
-          Frequently Asked Questions (FAQ)
-        </Typography>
-        {faq
-          .filter((q) => q.question && q.answer)
-          .map((q) => (
-            <Accordion key={q.id}>
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography variant="h6">{q.question}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>{q.answer}</Typography>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-      </Container>
-    </Box>
+    <StyledContainer maxWidth="md">
+      <StyledTitle variant="h4" gutterBottom>
+        Frequently Asked Questions
+      </StyledTitle>
+      {loading ? (
+        <StyledBox>
+          <CircularProgress />
+        </StyledBox>
+      ) : faq.length > 0 ? (
+        faq.map((q) => (
+          <AccordionStyled key={q.id}>
+            <StyledSummary expandIcon={<ExpandMoreIconStyled />}>
+              <Typography variant="h6">{q.question}</Typography>
+            </StyledSummary>
+            <StyledDetails>
+              <Typography>{q.answer}</Typography>
+            </StyledDetails>
+          </AccordionStyled>
+        ))
+      ) : (
+        <Typography>No FAQ found.</Typography>
+      )}
+    </StyledContainer>
   );
 }
