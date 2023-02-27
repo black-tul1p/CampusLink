057bd34ac7fce63db75be61c0206dcab547bc2a6
jjack100 <jacks688@purdue.edu>
2023-02-26 22:48:20 -0500

    Integrate instructor casslist page with backend

--- src/Pages/Classlist.js
+++ src/Pages/Classlist.js
@@ -1,11 +1,43 @@
 import "./Classlist.css";
+import {getStudentIdByEmail} from '../Backend/student'
+import {
+  collection,
+  doc,
+  addDoc,
+  deleteDoc,
+  getDocs,
+  query,
+  data,
+  where,
+} from "@firebase/firestore";
+import { firestore } from "../Backend/firebase";
 import logo from '../images/campuslink_banner.png'
 import ProfilePic from '../images/default_profile_picture.png'
 import LogoBanner from '../Components/LogoBanner.js'
 import { useState } from "react";
+import { useEffect } from "react";
+
+async function getStudents() {
+  const students = await getDocs(collection(firestore, 'students'))
+  return students
+}
 
 function Classlist() {
   const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudents() 
+    .then((studentSet) => {
+      let studentList = []
+      studentSet.forEach((doc) => {
+        studentList.push({firstName: doc.data().firstName,
+                          lastName:  doc.data().lastName,
+                          email:     doc.data().email});
+      });
+      setStudents(studentList);
+    });
+  }, []);
+
   return (
     <div className="Registration-page">
       <LogoBanner></LogoBanner>
@@ -16,8 +48,8 @@ function Classlist() {
         <button className="add-button" onClick={() => {
           setStudents([
             ...students, {
-             firstname: "Firstname",
-             lastname: "Lastname",
+             firstName: "Firstname",
+             lastName: "Lastname",
              email: "example@gmail.com"
             }
           ]);
@@ -36,7 +68,7 @@ function Classlist() {
                 <td className="profile-pic-column">
                   <img className="profile-pic" src={ProfilePic}></img>
                 </td>
-                <td>{student.lastname}, {student.firstname}</td>
+                <td>{student.lastName}, {student.firstName}</td>
                 <td>{student.email}</td>
               </tr>
             ))}
@@ -52,15 +84,15 @@ function StudentInfoRow(props) {
   return (
     <tr>
       <td className="profile-pic"></td>
-      <td>{props.lastname}, {props.firstname}</td>
+      <td>{props.lastName}, {props.firstName}</td>
       <td>{props.email}</td>
     </tr>
   );
 }
 
 StudentInfoRow.defaultProps = {
-  firstname: "Firstname",
-  lastname: "Lastname",
+  firstName: "Firstname",
+  lastName: "Lastname",
   email: "example@gmail.com",
 }
 
