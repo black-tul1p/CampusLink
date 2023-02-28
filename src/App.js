f7dfbdc2aa307ce043637df17a3b82a4fef3c4f7
sparkle_biswas <sparkleb04@gmail.com>
2023-02-28 06:05:30 -0500

    displays all the courses in the database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import NavBar from './Components/NavBar';
-import LogoutContainer from './Components/LogoutContainer';
-import Homepage_Student from './Pages/Homepage_Student';
+import HomepageStudent from './Pages/HomepageStudent';
 import Settings from './Pages/Settings';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -9,10 +8,9 @@ function App() {
     <div ClassName='App'>
       <Router>
       <NavBar />
-      {/* <LogoutContainer />    */}
       <Routes>
-        <Route exact path="/Homepage_Student" element={<Homepage_Student/>}/>
-        <Route exact path="/Settings" element={<Settings/>}/>
+      <Route exact path="/" element={<HomepageStudent/>}/>
+      <Route exact path="/Settings" element={<Settings/>}/>
       </Routes>
       </Router> 
        
