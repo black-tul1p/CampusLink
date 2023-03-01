8bb6cac1021f09a60463a90543dd178443492678
Divay Gupta <67044218+black-tul1p@users.noreply.github.com>
2023-03-01 03:27:55 -0500

    Improved Navbar and Homepage functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Admin from "./Components/Admin";
 import Register from "./Components/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
+import HomepageInstructor from "./Components/HomepageInstructor";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+`;
 
 function App() {
   return (
@@ -12,6 +18,15 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/join" element={<Register />} />
         <Route path="/nav" element={<NavBar />} />
+        <Route
+          path="/home"
+          element={
+            <Container>
+              <NavBar />
+              <HomepageInstructor />
+            </Container>
+          }
+        />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,79 +1,112 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import logo from "../Images/logo.jpg";
-import MenuIcon from "@mui/icons-material/Menu";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import SettingsIcon from "@mui/icons-material/Settings";
-import Person2Icon from "@mui/icons-material/Person2";
-import LogoutContainer from "./LogoutContainer";
-import "./NavBar.css";
+import React from "react";
+import styled from "@emotion/styled";
+import logo_mini from "../Assets/campuslink_logo.jpg";
+import { Notifications, Settings, AccountCircle } from "@mui/icons-material";
 
-function NavBar() {
-  const navigate = useNavigate();
+// CSS Styles
+const Sidebar = styled.div`
+  flex-shrink: 0;
+  width: 5em;
+  height: 100vh;
+  background-color: #20232a;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
 
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => {
-    setOpen(!open);
-  };
-  const [click, setClick] = useState(false);
-  const handleClick = () => {
-    setClick(!click);
-  };
+const SidebarItem = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 4em;
+  color: #fff;
+  cursor: pointer;
+  transition: transform 0.15s ease-out;
+  &:hover {
+    transform: scale(1.5);
+  }
+`;
 
+const SidebarText = styled.span`
+  font-size: 1.25em;
+  font-weight: 500;
+  margin-left: 1em;
+
+  @media (max-width: 768px) {
+    font-size: 1em;
+    margin-left: 0.5em;
+  }
+`;
+
+const SidebarDivider = styled.div`
+  flex-shrink: 0;
+  height: 1px;
+  background-color: #3c3f4c;
+  margin: 0.25em 1em;
+`;
+
+const SidebarRow = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: auto;
+  margin-bottom: 1em;
+`;
+
+const SidebarButton = styled.div`
+  display: flex;
+  align-items: center;
+  height: 3em;
+  padding: 0 25%;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const SidebarIcon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2.5em;
+  height: 2.5em;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.1);
+  &:hover {
+    background-color: teal;
+  }
+`;
+
+const NavBar = () => {
   return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <div className="logo-container">
-            <img src={logo} className="logo" alt="logo" />
-          </div>
-          <div
-            className="menu-logo-container"
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            <MenuIcon fontSize="large" sx={{ color: "#F0F8FF" }} />
-          </div>
-          <div onClick={handleOpen} className="notification-logo-container">
-            <NotificationsIcon fontSize="large" sx={{ color: "#F0F8FF" }} />
-            {/* <div className = "notification-dropdown">
-                        {open ? (
-                            <ul className = "notifications-list">
-                                <li className="notification">
-                                    <button>Notification 1</button>
-                                </li>
-                                <li>
-                                    <button>Notification 2</button>
-                                </li>
-                            </ul>
-                        ): null}
-                    </div> */}
-          </div>
-          <div
-            className="settings-logo-container"
-            onClick={() => {
-              navigate("/settings");
-            }}
-          >
-            <SettingsIcon fontSize="large" sx={{ color: "#F0F8FF" }} />
-          </div>
-          <div
-            id="user-icon"
-            onClick={handleClick}
-            className="user-logo-container"
-          >
-            {click ? (
-              <Person2Icon fontSize="large" sx={{ color: "#F0F8FF" }} />
-            ) : (
-              <Person2Icon fontSize="large" sx={{ color: "#F0F8FF" }} />
-            )}
-          </div>
-        </div>
-        {click && <LogoutContainer />}
-      </nav>
-    </>
+    <Sidebar>
+      <SidebarItem>
+        <img src={logo_mini} style={{ width: "2.5em" }} />
+        {/* <SidebarText>Home</SidebarText> */}
+      </SidebarItem>
+      <SidebarDivider />
+      <SidebarRow>
+        <SidebarButton>
+          <SidebarIcon>
+            <Notifications style={{ color: "#fff" }} />
+          </SidebarIcon>
+          {/* <SidebarText>Notifications</SidebarText> */}
+        </SidebarButton>
+        <SidebarButton>
+          <SidebarIcon>
+            <Settings style={{ color: "#fff" }} />
+          </SidebarIcon>
+          {/* <SidebarText>Settings</SidebarText> */}
+        </SidebarButton>
+        <SidebarButton>
+          <SidebarIcon>
+            <AccountCircle style={{ color: "#fff" }} />
+          </SidebarIcon>
+          {/* <SidebarText>User</SidebarText> */}
+        </SidebarButton>
+      </SidebarRow>
+    </Sidebar>
   );
-}
+};
 
 export default NavBar;
