3249832f01ab4c590713002449a2aaba2efa76a3
sparkle_biswas <sparkleb04@gmail.com>
2023-02-28 19:45:02 -0500

    formatted the navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 import logo from '../Images/logo.jpg';
 import menu_logo from '../Images/menu_logo.jpg';
 import settings_logo from '../Images/settings_logo.jpg';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Person2Icon from '@mui/icons-material/Person2';
+
 import notification_logo from '../Images/notification_logo.jpg';
 import user_logo from '../Images/user_logo.jpg';
 import LogoutContainer from './LogoutContainer';
@@ -28,10 +33,11 @@ function NavBar() {
                 </div>
                 <div className = "menu-logo-container">
                     <Link to="/">
-                        <img src={menu_logo} className = "menu_logo" alt="menu"/>
+                        <MenuIcon fontSize="large" sx={{ color: "#F0F8FF" }}/>
                     </Link>
                 </div>
                 <div onClick={handleOpen} className = "notification-logo-container">
+                    <NotificationsIcon fontSize="large" sx={{ color: "#F0F8FF" }} />
                     {/* <div className = "notification-dropdown">
                         {open ? (
                             <ul className = "notifications-list">
@@ -44,15 +50,14 @@ function NavBar() {
                             </ul>
                         ): null}
                     </div> */}
-                    <img src={notification_logo} className = "notification_logo" alt="notifications"/>
                 </div>
                 <div className = "settings-logo-container">
                     <Link to="/Settings">
-                        <img src={settings_logo} className = "settings_logo" alt="settings"/> 
+                        <SettingsIcon fontSize="large" sx={{ color: "#F0F8FF" }} />
                     </Link>
                 </div>
                 <div onClick= {handleClick} className = "user-logo-container">
-                    {click ? <img src={user_logo} className = "user_logo" alt="user profile"/>: <img src={user_logo} className = "user_logo" alt="user profile"/>}
+                    {click ? <Person2Icon fontSize="large" sx={{ color: "#F0F8FF" }}/>: <Person2Icon fontSize="large" sx={{ color: "#F0F8FF" }} />}
                 </div>  
             </div>
             {click && <LogoutContainer/>} 
