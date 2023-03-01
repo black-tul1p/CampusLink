f001f3924ff0d87c283f104c51bd85b570b424f4
Divay Gupta <67044218+black-tul1p@users.noreply.github.com>
2023-03-01 04:51:44 -0500

    Added loading screen

--- src/Components/HomepageInstructor.js
+++ src/Components/HomepageInstructor.js
@@ -3,6 +3,7 @@ import "./HomepageStudent";
 import { getAllCourses } from "../Backend/course";
 import { useState, useEffect } from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { CircularProgress } from "@mui/material";
 
 function HomepageInstructor() {
   const [courses, setCourses] = useState([]);
@@ -30,38 +31,46 @@ function HomepageInstructor() {
         <p>My Courses</p>
         <div className="divider"></div>
       </div>
-      <div className="homepage-student-container">
-        {loading ? (
-          <div
-            className="add-course-container"
-            style={{ backgroundColor: "none" }}
-          >
+      {loading ? (
+        <div
+          style={{
+            flexGrow: 1,
+            marginTop: "30vh",
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      ) : (
+        <div className="homepage-student-container">
+          {courses.length > 0 ? (
+            courses
+              .filter(
+                (course) =>
+                  course.description && course.courseId && course.courseTitle
+              )
+              .map((course) => (
+                <div className="course-container">
+                  <div className="course-container-top">
+                    <h3>
+                      {course.courseTitle} {course.courseId} :
+                    </h3>
+                    <h3>{course.description}</h3>
+                  </div>
+                  <div className="course-container-bottom"></div>
+                </div>
+              ))
+          ) : (
+            <h1>Username is not teaching any courses!</h1>
+          )}
+          <div onClick={showAlert} className="add-course-container">
             <AddCircleIcon fontSize="large" />
           </div>
-        ) : courses.length > 0 ? (
-          courses
-            .filter(
-              (course) =>
-                course.description && course.courseId && course.courseTitle
-            )
-            .map((course) => (
-              <div className="course-container">
-                <div className="course-container-top">
-                  <h3>
-                    {course.courseTitle} {course.courseId} :
-                  </h3>
-                  <h3>{course.description}</h3>
-                </div>
-                <div className="course-container-bottom"></div>
-              </div>
-            ))
-        ) : (
-          <h1>Username is not teaching any courses!</h1>
-        )}
-        <div onClick={showAlert} className="add-course-container">
-          <AddCircleIcon fontSize="large" />
         </div>
-      </div>
+      )}
     </div>
   );
 }
