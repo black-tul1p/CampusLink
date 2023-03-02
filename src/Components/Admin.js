0cb7f69b681d2ea94328b3c4465b6f396bfe2850
Rudis Sprenne <70488838+rsprenne@users.noreply.github.com>
2023-03-01 21:07:58 -0500

    Added tab menu to Admin page

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -5,11 +5,52 @@ import { useSearchParams } from "react-router-dom";
 import "./Classlist.css";
 import { Box, Tab, Tabs, Typography } from "@mui/material";
 import styled from "@emotion/styled";
+import { PropTypes } from "prop-types";
+
+//From Material UI website example
+
+function TabPanel(props) {
+    const {children, value, index, ...other } = props;
+
+    return (
+        <div
+        	role="tabpanel"
+        	hidden={value !== index}
+        	id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+        	{...other}
+        >
+            {value === index && (
+                <Box sx={{ p:3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node, 
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function allyProps(index) {
+	return {
+		id: `simple-tab-${index}`, 
+		'aria-controls': `simple-tabpanel-${index}`,
+	};
+}
 
 export default function Admin() {
   const [instructors, setInstructors] = useState([]);
   const [courseData, setCourseData] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+	setValue(newValue);
+  }
 
   useEffect(() => {
     //Get students and add to list
@@ -31,13 +72,13 @@ export default function Admin() {
     <Box sx={{ width: '100%' }}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
             <Tabs value={value} onChange={handleChange} >
-                <Tab label="Students" />
-                <Tab label="Instructors" />
-                <Tab label="Pending Requests" />
+                <Tab label="Students" {...allyProps(0)} />
+                <Tab label="Instructors" {...allyProps(1)} />
+                <Tab label="Pending Requests" {...allyProps(2)} />
             </Tabs>
         </Box>
         <TabPanel value={value} index={0}>
-            Student
+            This is changed
         </TabPanel>
         <TabPanel value={value} index={1}>
             Instructor 
@@ -63,25 +104,4 @@ InstructorInfoRow.defaultProps = {
   firstName: "Firstname",
   lastName: "Lastname",
   email: "example@gmail.com",
-}
-
-//From Material UI website example
-
-function TabPanel(props) {
-    const {children, value, index, ...other } = props;
-
-    return (
-        <div
-        role="tabpanel"
-        hidden={vaule !== index}
-        id={`simple-tabpanel-${index}`}
-        {...other}
-        >
-            {value === index && (
-                <Box sx={{ p:3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
 }
