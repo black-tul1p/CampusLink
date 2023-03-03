93f2de630dc1059059d0d6ba10b010885ef0b180
Divay Gupta <67044218+black-tul1p@users.noreply.github.com>
2023-03-02 19:40:18 -0500

    Fixed merge conflict

--- src/Backend/instructor.js
+++ src/Backend/instructor.js
@@ -1,69 +1,50 @@
-import {
-    collection,
-    doc,
-    setDoc,
-    getDocs,
-    query,
-    where,
-    getDoc,
-    addDoc,
-  } from "@firebase/firestore";
-  import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    deleteUser,
-    signInWithEmailAndPassword,
-    signOut,
-  } from "firebase/auth";
-  import { auth, firestore } from "./firebase";
-  import { getCourseDetailsById } from "./course";
-import { async } from "@firebase/util";
+import { collection, getDocs, query, where, addDoc } from "@firebase/firestore";
+import { firestore } from "./firebase";
+import { getCourseDetailsById } from "./course";
 
 export async function createInstructor(first, last, email, password) {
-    let data = {
-        firstName: first, 
-        lastName: last, 
-        email: email, 
-        pass: password, 
-        accepted: false,
-        courses: []
-    };
+  let data = {
+    firstName: first,
+    lastName: last,
+    email: email,
+    pass: password,
+    accepted: false,
+    courses: [],
+  };
 
-    try {
-        const docRef = await addDoc(collection(firestore, "instructors"), data);
-        console.log("Instructor added with ID: ", docRef.id);
-    } catch (e) {
-        console.error("Error adding instructor: ", data);
-    }
+  try {
+    const docRef = await addDoc(collection(firestore, "instructors"), data);
+    console.log("Instructor added with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding instructor: ", data);
+  }
 }
 
-export const getInstructorCourses = async() => {
+export const getInstructorCourses = async () => {
   try {
     var course = [];
-      
-      const getData = collection(firestore, "instructors");
-      
-      const snapshot = query(getData, where("email", "==", "test@test.edu"));
-      const querySnapshot = await getDocs(snapshot);
 
-      if (querySnapshot.docs.length === 0) {
-        alert("No instructor found with email");
-      }
-      
-      
-      querySnapshot.forEach(async (doc) => {
-        //console.log(doc.data().courses)
-        
-        doc.data().courses.forEach(async (doc2) => {
-          const sTest = new String(doc2.path);
-          const courseIDF = sTest.slice(8);
-          //console.log(courseIDF);
-          //console.log(doc.data().courses[0].path);
-        
-          
-            await getCourseDetailsById(courseIDF).then((res) => {
-            //console.log(res);
-            /*course.push({
+    const getData = collection(firestore, "instructors");
+
+    const snapshot = query(getData, where("email", "==", "test@test.edu"));
+    const querySnapshot = await getDocs(snapshot);
+
+    if (querySnapshot.docs.length === 0) {
+      alert("No instructor found with email");
+    }
+
+    querySnapshot.forEach(async (doc) => {
+      //console.log(doc.data().courses)
+
+      doc.data().courses.forEach(async (doc2) => {
+        const sTest = new String(doc2.path);
+        const courseIDF = sTest.slice(8);
+        //console.log(courseIDF);
+        //console.log(doc.data().courses[0].path);
+
+        await getCourseDetailsById(courseIDF).then((res) => {
+          //console.log(res);
+          /*course.push({
               courseTitle: res.courseTitle,
               courseId: res.courseId,
               credit: res.credit,
@@ -72,27 +53,16 @@ export const getInstructorCourses = async() => {
               registeredStudents: res.registeredStudents,
               description: res.description,
             }); */
-            if (res) course.push(res);
-          });
-          
-          
-         // console.log(course.length)
+          if (res) course.push(res);
+        });
+
+        // console.log(course.length)
       });
-      console.log(course );
-      }); 
-      //console.log("All courses fetched:", course.length);
-      return course;
-    } catch (error) {
-      throw new Error("Error fetching courses:", error);
-    }
-  };
-
-
-
-    try {
-        const docRef = await addDoc(collection(firestore, "instructors"), data);
-        console.log("Instructor added with ID: ", docRef.id);
-    } catch (e) {
-        console.error("Error adding instructor: ", data);
-    }
-}
+      console.log(course);
+    });
+    //console.log("All courses fetched:", course.length);
+    return course;
+  } catch (error) {
+    throw new Error("Error fetching courses:", error);
+  }
+};
