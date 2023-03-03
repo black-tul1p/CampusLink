e896fb2667eda06caa69e631eb48398805d80b26
jjack100 <jacks688@purdue.edu>
2023-03-03 14:20:16 -0500

    fix student remove button

--- src/Components/CoursePages/Classlist.js
+++ src/Components/CoursePages/Classlist.js
@@ -1,19 +1,18 @@
 import "../Classlist.css";
-import {doc, getDoc} from "@firebase/firestore";
+import {doc, getDoc, updateDoc, arrayRemove, collection} from "@firebase/firestore";
 import { firestore } from "../../Backend/firebase";
 import ProfilePic from '../../images/default_profile_picture.png'
 import { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Classlist() {
   const [students, setStudents] = useState([]);
   const [courseData, setCourseData] = useState([]);
+  const [courseDocId, setCourseDocId] = useState("");
   const location = useLocation();
 
-  //Initialize data which comes from the database
-  useEffect(() => {
-    // Get course title and description
-    const courseID = location.state?.courseId;
+  const updateClassList = (courseID) => {
     if (courseID === undefined) {
       console.log("Course not specified!");
     } else {
@@ -26,13 +25,20 @@ function Classlist() {
           //Add enrolled students to classlist
           const enrolled = courseDoc.data().enrolledStudents;
           Promise.all(enrolled.map(getDoc)).then(
-            stdnts => setStudents(stdnts.map(s => s.data()))
+            stdnts => setStudents(stdnts)
           ); 
         } else {
           console.log("Course not found!");
         }
       });
     }
+  }
+  //Initialize data which comes from the database
+  useEffect(() => {
+    // Get course title and description
+    const courseID = location.state?.courseId;
+    setCourseDocId(courseID);
+    updateClassList(courseID);    
   }, [location]);
 
   return (
@@ -64,11 +70,22 @@ function Classlist() {
 
             {students.map(student => (
               <tr>
-                <td className="profile-pic-column">
+                <td className="profile-pic-column" studentid={student.id} >
+                  <div className="remove-student-container" onClick={(event) => {
+                    const id = event.currentTarget.parentElement.getAttribute('studentid');
+                    console.log(courseDocId);
+                    const courseRef = doc(firestore, 'courses', courseDocId)
+                    const students = collection(firestore, "students");
+                    updateDoc(courseRef, {enrolledStudents: arrayRemove(doc(students, id))}).then(() => {
+                      updateClassList(courseDocId);  
+                    });
+                  }}>
+                       <DeleteIcon fontSize="large" />
+                    </div>
                   <img className="profile-pic" src={ProfilePic} alt="profile"></img>
                 </td>
-                <td>{student.lastName}, {student.firstName}</td>
-                <td>{student.email}</td>
+                <td>{student.data().lastName}, {student.data().firstName}</td>
+                <td>{student.data().email}</td>
               </tr>
             ))}
           </tbody>
