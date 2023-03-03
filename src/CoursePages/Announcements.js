f4560ee5c9744a9b27ed7335e08442ba523ebd69
sparkle_biswas <sparkleb04@gmail.com>
2023-03-03 04:48:53 -0500

    sends courseId to all pages

--- src/CoursePages/Announcements.js
+++ src/CoursePages/Announcements.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-import { useState, useEffect} from 'react'
 
 function Announcements() {
-  const location = useLocation();
-  const courseId = location.state?.courseId;
   return (
     <div>
     </div>
