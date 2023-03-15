ad95e86407cc90e9aa56deb593a8d6cede710597
sparkle_biswas <sparkleb04@gmail.com>
2023-03-15 16:32:36 -0400

    successfully reads user inputs and adds assignment to database

--- src/Components/CoursePages/Assignments.js
+++ src/Components/CoursePages/Assignments.js
@@ -3,11 +3,24 @@ import CourseNavBar from "../CourseNavBar";
 import { addAssignment } from "../../Backend/assigment";
 import "../../Styles/Assignments.css";
 import "../../Styles/App.css";
+import { useState} from "react";
 
-function assignments() {
-  const handleClick = () => {
-    addAssignment("Assignment 20", "Something something etc.", "");
+function Assignments() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [description, setDescription] = useState("");
+  const handleSubmit = () => {
+    const due = (date + " " + time);
+    addAssignment(title, description, due);
   }
+  const handleCancel = () => {
+    setTitle("");
+    setDate("");
+    setTime("");
+    setDescription("");
+  }
+
   return (
     <div style={{ width: "100%" }}>
       <CourseNavBar />
@@ -18,14 +31,37 @@ function assignments() {
         </div>
         <div className = "assignment-form-box">
             <label> Assignment Title </label>
-            <input placeholder="Assignment 1"/>    
+            <input placeholder="Assignment 1"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />    
             <label> Due Date </label>
-            <input placeholder="YYYY-MM-DD"/>    
+            <input placeholder="YYYY-MM-DD"
+              value={date}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+            />  
+            <label> Due At </label>
+            <input placeholder="23:59:00"
+              value={time}
+              onChange={(e) => {
+                setTime(e.target.value);
+              }}
+            />    
             <label> Assignment Description </label> 
-            <textarea rows="4" placeholder="Assignment Description goes Here."/>  
+            <textarea rows = "3"
+              placeholder="Assignment Description goes Here."
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />  
             <div className="button-box">
-              <button>Submit</button>
-              <button>Cancel</button>
+              <button onClick={handleSubmit}>Submit</button>
+              <button onClick={handleCancel}>Cancel</button>
             </div>
         </div>
       </div>
@@ -33,4 +69,4 @@ function assignments() {
   );
 }
 
-export default assignments;
+export default Assignments;
