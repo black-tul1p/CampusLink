e3d98119b2675f1a39b4516af5f83fd8879e8f40
Divay Gupta <67044218+black-tul1p@users.noreply.github.com>
2023-03-29 14:05:23 -0400

    Fix filename

--- src/Components/CoursePages/Assignments.js
+++ src/Components/CoursePages/Assignments.js
@@ -1,21 +1,23 @@
 import CourseNavBar from "../CourseNavBar";
-import { addAssignment, verifyInput, getAssigmentsByCourse } from "../../Backend/assigment";
+import {
+  addAssignment,
+  verifyInput,
+  getAssigmentsByCourse,
+} from "../../Backend/assignment";
 import "../../Styles/Assignments.css";
 import "../../Styles/App.css";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import React from "react";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { getUserRole } from "../../Backend/user";
 import ErrorBox from "../Error";
 import { useLocation } from "react-router-dom";
 
-
 function Assignments() {
-
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
@@ -27,8 +29,6 @@ function Assignments() {
   const [courseDocId, setCourseDocId] = useState("");
   const [assignments, setAssignments] = useState([]);
   const location = useLocation();
-
-
 
   useEffect(() => {
     async function fetchData() {
@@ -51,21 +51,21 @@ function Assignments() {
 
   const toggle = () => {
     setOpen(!open);
-    if(open === false) {
+    if (open === false) {
       setTitle("");
       setDate("");
       setTime("");
       setDescription("");
       setOpen1(false);
     }
-  }
+  };
 
   const firstToggle = () => {
     setOpen1(!open1);
-    if(!open1) {
+    if (!open1) {
       handleCancel();
     }
-  }
+  };
 
   const handleSubmit = () => {
     if (!verifyInput(title, description, date, time)) {
@@ -74,155 +74,140 @@ function Assignments() {
       setTimeout(() => {
         setError("");
       }, 1500);
-        return;
+      return;
     } else {
       const due = date + " " + time;
       addAssignment(title, description, due, courseDocId);
       alert("New Assignment Added!");
       handleCancel();
     }
-    
-  }
+  };
   const handleCancel = () => {
     setTitle("");
     setDate("");
     setTime("");
     setDescription("");
     setOpen(false);
-  }
-
+  };
 
   const handleUpload = () => {
     console.log("File Changed");
-  }
+  };
 
   return (
-    <div className = "main-box" style={{ width: "100%" }}>
+    <div className="main-box" style={{ width: "100%" }}>
       <CourseNavBar />
       {error && <ErrorBox text={error} />}
-      <div className = "assignment-box">
+      <div className="assignment-box">
         {role === "student" ? (
           <div className="inner-assignment-box">
-            <div className ="header-box">
+            <div className="header-box">
               <div className="header-titles">
                 <p>Assignments</p>
                 <p style={{ fontStyle: "italic" }}>Student View</p>
               </div>
               <div className="header-divider"></div>
             </div>
-            <div className="assignments-bar"  onClick={firstToggle}>
-              <p style={{fontSize:"1.4em"}}>
-                All Assignments
-              </p>
-              {open1 ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
+            <div className="assignments-bar" onClick={firstToggle}>
+              <p style={{ fontSize: "1.4em" }}>All Assignments</p>
+              {open1 ? <ExpandLessIcon /> : <ExpandMoreIcon />}
             </div>
             {open1 && (
               <div className="all-assigments-box">
                 {assignments.length > 0 ? (
-                    assignments
-                      .filter(
-                        (assignment) =>
-                          assignment.title
-                      )
-                      .map((assignment) => (
-                        <div className = 'assignment-list-box'>
-                          <NavigateNextIcon/>
-                          <label>{assignment.title}</label>
-                        </div>
-                      ))
-                ):
-                (<label>No Assignments</label>) 
-                }
-                
+                  assignments
+                    .filter((assignment) => assignment.title)
+                    .map((assignment) => (
+                      <div className="assignment-list-box">
+                        <NavigateNextIcon />
+                        <label>{assignment.title}</label>
+                      </div>
+                    ))
+                ) : (
+                  <label>No Assignments</label>
+                )}
               </div>
             )}
           </div>
-        ): role === "instructor" ? (
+        ) : role === "instructor" ? (
           <div className="inner-assignment-box">
-            <div className ="header-box">
+            <div className="header-box">
               <div className="header-titles">
-              <p>Assignments</p>
-              <p style={{ fontStyle: "italic" }}>Instructor View</p>
+                <p>Assignments</p>
+                <p style={{ fontStyle: "italic" }}>Instructor View</p>
               </div>
               <div className="header-divider"></div>
             </div>
-            <div className="assignments-bar"  onClick={firstToggle}>
-              <p style={{fontSize:"1.3em"}}>
-                All Assignments
-              </p>
-              {open1 ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
+            <div className="assignments-bar" onClick={firstToggle}>
+              <p style={{ fontSize: "1.3em" }}>All Assignments</p>
+              {open1 ? <ExpandLessIcon /> : <ExpandMoreIcon />}
             </div>
             {open1 && (
               <div className="all-assigments-box">
                 {assignments.length > 0 ? (
-                    assignments
-                      .filter(
-                        (assignment) =>
-                          assignment.title
-                      )
-                      .map((assignment) => (
-                        <div className = 'assignment-list-box'>
-                          <NavigateNextIcon/>
-                          <label>{assignment.title}</label>
-                        </div>
-                      ))
-                ):
-                (<label>No Assignments</label>) 
-                }
-                
+                  assignments
+                    .filter((assignment) => assignment.title)
+                    .map((assignment) => (
+                      <div className="assignment-list-box">
+                        <NavigateNextIcon />
+                        <label>{assignment.title}</label>
+                      </div>
+                    ))
+                ) : (
+                  <label>No Assignments</label>
+                )}
               </div>
             )}
             <div className="create-assignment-bar" onClick={toggle}>
-              <p style={{fontSize:"1.3em"}}>
-                Create An Assignment
-              </p>
-              {open ? <RemoveCircleIcon/> : <AddCircleIcon />}
+              <p style={{ fontSize: "1.3em" }}>Create An Assignment</p>
+              {open ? <RemoveCircleIcon /> : <AddCircleIcon />}
             </div>
             {open && (
-              <div className = "assignment-form-box">
+              <div className="assignment-form-box">
                 <label> Assignment Title </label>
-                <input placeholder="Assignment 1"
+                <input
+                  placeholder="Assignment 1"
                   value={title}
                   onChange={(e) => {
                     setTitle(e.target.value);
                   }}
-                />    
+                />
                 <label> Due Date </label>
-                <input placeholder="YYYY-MM-DD"
+                <input
+                  placeholder="YYYY-MM-DD"
                   value={date}
                   onChange={(e) => {
                     setDate(e.target.value);
                   }}
-                />  
+                />
                 <label> Due At </label>
-                <input placeholder="23:59:00"
+                <input
+                  placeholder="23:59:00"
                   value={time}
                   onChange={(e) => {
                     setTime(e.target.value);
                   }}
-                />    
-                <label> Assignment Description </label> 
-                <textarea rows = "3"
+                />
+                <label> Assignment Description </label>
+                <textarea
+                  rows="3"
                   placeholder="Assignment Description goes Here."
                   value={description}
                   onChange={(e) => {
                     setDescription(e.target.value);
                   }}
                 />
-                <label> Upload PDFs/Images </label> 
-                <input type="file"/>
+                <label> Upload PDFs/Images </label>
+                <input type="file" />
                 <div className="button-box">
-                  <button onClick={handleSubmit}>
-                    Submit
-                  </button>
+                  <button onClick={handleSubmit}>Submit</button>
                   <button onClick={handleCancel}>Cancel</button>
                 </div>
               </div>
             )}
           </div>
-        ): null}
-      </div>  
-         
+        ) : null}
+      </div>
     </div>
   );
 }
